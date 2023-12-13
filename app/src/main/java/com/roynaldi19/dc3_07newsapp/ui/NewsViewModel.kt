d0bfa9297631f3a9989a0fa219ba120c5a8c68d4
roynaldi19 <roynaldi19@gmail.com>
2023-12-13 20:05:09 +0700

    add bookmared

--- app/src/main/java/com/roynaldi19/dc3_07newsapp/ui/NewsViewModel.kt
+++ app/src/main/java/com/roynaldi19/dc3_07newsapp/ui/NewsViewModel.kt
@@ -2,7 +2,18 @@ package com.roynaldi19.dc3_07newsapp.ui
 
 import androidx.lifecycle.ViewModel
 import com.roynaldi19.dc3_07newsapp.data.NewsRepository
+import com.roynaldi19.dc3_07newsapp.data.local.entity.NewsEntity
 
 class NewsViewModel(private val newsRepository: NewsRepository): ViewModel() {
     fun getHeadlineNews() = newsRepository.getHeadlineNews()
+
+    fun getBookmarkedNews() = newsRepository.getBookmarkedNews()
+
+    fun saveNews(news: NewsEntity) {
+        newsRepository.setBookmarkedNews(news, true)
+    }
+
+    fun deleteNews(news: NewsEntity){
+        newsRepository.setBookmarkedNews(news, false)
+    }
 }
